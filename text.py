f6f23037211f369b133e07c221643986058bd1ef
Dan Frunza <100516627+Behemoth5949@users.noreply.github.com>
2025-02-01 04:39:48 -0800

    Update text.py

---
+++ text.py
@@ -0,0 +1,7 @@
+#echo " Ayo" >> README.md
+#git add README.md
+#git commit -m "first commit"
+#git branch -M main
+#git remote add origin https://github.com/Behemoth5949/Ayo.git
+#git push -u origin main
+print("Hello world")
